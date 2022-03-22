28e1b6c75858f07fc973549823d9798d2d62d48d
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-03-22 17:26:46 +0100

    v0.1.1
    Edit animation.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+// Get all cards elements as an array
 const cards = gsap.utils.toArray('.card');
 
+// Loop through all cards
 cards.forEach((card, index) => {
 
-    const img = card.lastElementChild
+    // For each card get specific elements
+    const type = card.children[1];
+    const bottom = card.children[2];
+    const img = card.lastElementChild;
 
+    // For each card create a timeline to animate elemnts
+    // Add a delay depending on index to create a stagger effect
     var tl = gsap.timeline().delay(index * .4)
     tl.from(card, { autoAlpha: 0, yPercent: 50 })
         .from(img, { autoAlpha: 0 })
+        .from(type, { autoAlpha: 0, yPercent: -100 })
+        .from(bottom, { autoAlpha: 0, yPercent: 30 }, "<");
 
-
-})
+});
